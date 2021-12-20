30f7c4587f91b8bf1ff6409bb143ed85ae1eaabc
nityanandabehera85132@gmail.com <nityanandabehera85132@gmail.com>
2021-12-20 21:12:45 +0530

    pagination update

--- shop/views.py
+++ shop/views.py
@@ -8,6 +8,10 @@ def index(request):
     item_name=request.GET.get('item_name')
     if item_name !='' and item_name is not None:
         product_objects=product_objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_objects,4)
+    page_number=request.GET.get('page')
+    product_objects=paginator.get_page(page_number)
+
 
     return render(request,'index.html',{'product_objects':product_objects})
 
